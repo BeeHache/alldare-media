263dfcbddce07bf431c8d8179510b873a77d271b
BeeHache <bh@blackhacker.net>
2026-06-12 00:45:49 -0400

    fix: configure oauth2ResourceServer in SecurityConfig to allow JWT principal resolving in production

--- src/main/java/online/alldare/media/config/SecurityConfig.java
+++ src/main/java/online/alldare/media/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package online.alldare.media.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +18,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
-                );
+                )
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
